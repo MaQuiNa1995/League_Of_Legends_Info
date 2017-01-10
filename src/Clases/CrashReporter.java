5dd36b7370bb5d8a4e1497f0615ecb8432db5646
MaQuiNa1995 <maquina1995@gmail.com>
2017-01-10 12:16:12 +0100

    Actualizacion de la clase CrashReporter
    implementacion del envío del mail con las excepciones

--- src/Clases/CrashReporter.java
+++ src/Clases/CrashReporter.java
@@ -8,23 +8,31 @@ package Clases;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Properties;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
 import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
 
 /**
@@ -37,6 +45,8 @@ public final class CrashReporter extends JFrame implements ActionListener {
 
     String claseActual;
 
+    JTextArea descProblemaText;
+
     public CrashReporter(String claseActual) {
         this.claseActual = claseActual;
         definirVentana();
@@ -47,8 +57,7 @@ public final class CrashReporter extends JFrame implements ActionListener {
     void definirVentana() {
 
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        setSize(300, 300);
-        //setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setSize(400, 300);
         setTitle("Crash Reporter");
 
     }
@@ -67,7 +76,7 @@ public final class CrashReporter extends JFrame implements ActionListener {
         descProblemaPanel.setLayout(new BoxLayout(descProblemaPanel, BoxLayout.PAGE_AXIS));
 
         JLabel descProblemaEtiqueta = new JLabel("Descripción Del Problema");
-        JTextArea descProblemaText = new JTextArea(300, 200);
+        descProblemaText = new JTextArea(300, 200);
         descProblemaText.setLineWrap(true);
 
         descProblemaPanel.add(descProblemaEtiqueta);
@@ -77,6 +86,7 @@ public final class CrashReporter extends JFrame implements ActionListener {
         botonPanel.setLayout(new BoxLayout(botonPanel, BoxLayout.PAGE_AXIS));
 
         JButton botonEnviar = new JButton("Enviar");
+        botonEnviar.addActionListener(this);
 
         botonPanel.add(botonEnviar);
 
@@ -100,16 +110,71 @@ public final class CrashReporter extends JFrame implements ActionListener {
     // TODO aqui falta excepcion 
     public static void meterCadena(String cadenaMeter, String NombreFichero) {
 
-        try (FileWriter Escribir_Fichero = new FileWriter(Rutas.rutaInstalacion + "/InfoLol/Logs/" + NombreFichero+".txt", false)) {
+        try (FileWriter Escribir_Fichero = new FileWriter(Rutas.rutaInstalacion + "/InfoLol/Logs/" + NombreFichero + ".txt", false)) {
             Escribir_Fichero.write(cadenaMeter + "\n");
         } catch (IOException error) {
+        }
+    }
+
+    public void envioCorreo(String asunto, String mensaje) {
+
+        String miCorreo = "maquina1995prueba@gmail.com";
+        String destinatario = "maquina1995@gmail.com";
+        String miContra = "1995prueba1995";
+        String servidorSMTP = "smtp.gmail.com";
+        String puertoEnvio = "587";
+
+        Properties props = new Properties();
+
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", servidorSMTP);
+        props.put("mail.smtp.port", puertoEnvio);
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(miCorreo, miContra);
+            }
+        });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(miCorreo));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(destinatario));
+            message.setSubject(asunto);
+
+            MimeMultipart multiParte = new MimeMultipart();
+
+            BodyPart texto = new MimeBodyPart();
+            texto.setText(mensaje);
+            // de aqui a
+            String adjunto = Rutas.rutaInstalacion + "/InfoLol/Logs/" + claseActual + ".txt";
+
+            DataSource fuente = new FileDataSource(adjunto);
+            texto.setDataHandler(new DataHandler(fuente));
+            texto.setFileName(adjunto);
+
+// aqui
+            multiParte.addBodyPart(texto);
+            message.setContent(multiParte);
+
+            Transport.send(message);
+
+        } catch (MessagingException ex) {
+
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent ae) {
         if (ae.getActionCommand().equalsIgnoreCase("Enviar")) {
-
+            envioCorreo("Crasheo En La Clase: ".concat(claseActual), descProblemaText.getText());
+            System.exit(1);
         }
     }
+
 }
